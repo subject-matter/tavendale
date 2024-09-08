2df601bb4d3e0d583e9cfd0b842d1dca6213ab29
Braedne <braedenltd@gmail.com>
2024-09-09 09:10:25 +1000

    fix logo

--- public/payments/payment-successful.php
+++ public/payments/payment-successful.php
@@ -1,54 +1,56 @@
 <html>
+
 <head>
-<title>Payments | Tavendale and Partners</title>
-<meta type="description" cotnent="Tavendale and Partners" />
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,500,700" rel="stylesheet">
-<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
-<link href="/payments/css/style.css" rel="stylesheet" />
+    <title>Payments | Tavendale and Partners</title>
+    <meta type="description" cotnent="Tavendale and Partners" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,500,700" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
+    <link href="/payments/css/style.css" rel="stylesheet" />
 
 </head>
 
 <body>
 
-<section class="background">
+    <section class="background">
 
-<div class="white-container">
+        <div class="white-container">
 
-    <div class="form-container">
+            <div class="form-container">
 
-        <header>
-            <img src="/payments/img/tp-logo.png" alt="Tavendale and Partners Logo" />
-            <a href="https://www.tp.co.nz">BACK TO HOME PAGE</a>
-        </header>
+                <header>
+                    <img src="/payments/img/tp-logo.svg" alt="Tavendale and Partners Logo" />
+                    <a href="https://www.tp.co.nz">BACK TO HOME PAGE</a>
+                </header>
 
-        <div class="content">
-            <h1>Payment Successful</h1>
-            <p>Thank you, your payment has been received. You will receive a comfirmation email shortly.</p>
-            <a href="https://www.tp.co.nz" class="return-button">Back to home page</a>
-            <a href="/payments/" class="return-button button-margin">Make another payment</a>
-        </div>
+                <div class="content">
+                    <h1>Payment Successful</h1>
+                    <p>Thank you, your payment has been received. You will receive a comfirmation email shortly.</p>
+                    <a href="https://www.tp.co.nz" class="return-button">Back to home page</a>
+                    <a href="/payments/" class="return-button button-margin">Make another payment</a>
+                </div>
 
-        <div class="page-divider"></div>
+                <div class="page-divider"></div>
 
-        <footer>
+                <footer>
 
 
-            <div class="social-icons">
-                <a href="https://www.linkedin.com/company-beta/1440983/" target="_blank" rel="noopener"><img src="/payments/img/linkedin-icon.png" alt="LinkedIn Icon" /></a>
-                <a href="https://www.facebook.com/tavendaleandpartners/" target="_blank" rel="noopener"><img src="/payments/img/facebook-icon.png" alt="Facebook Icon" /></a>
+                    <div class="social-icons">
+                        <a href="https://www.linkedin.com/company-beta/1440983/" target="_blank" rel="noopener"><img src="/payments/img/linkedin-icon.png" alt="LinkedIn Icon" /></a>
+                        <a href="https://www.facebook.com/tavendaleandpartners/" target="_blank" rel="noopener"><img src="/payments/img/facebook-icon.png" alt="Facebook Icon" /></a>
+                    </div>
+
+                    <p>Contact us at <a href="mailto:you+us@tp.co.nz">you+us@tp.co.nz</a></p>
+                </footer>
+
             </div>
 
-            <p>Contact us at <a href="mailto:you+us@tp.co.nz">you+us@tp.co.nz</a></p>
-        </footer>
+        </div>
 
-    </div>
+    </section>
 
-</div>
-
-</section>
-
-<script src="https://js.stripe.com/v3/"></script>
-<script src="/payments/js/payment.js"></script>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="/payments/js/payment.js"></script>
 
 </body>
+
 </html>
